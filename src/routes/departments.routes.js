d512a53c3c4288c7915954f63d7230aa58b1d45c
ulyanovskk <mr1english4@gmail.com>
2022-01-28 22:44:03 +0300

    Refactor

--- src/routes/departments.routes.js
+++ src/routes/departments.routes.js
@@ -4,8 +4,7 @@ const router = express.Router()
 router.get('/', async (req, res) => {
     const departments = req.app.departments
     if (departments === undefined) console.log("No departments collection in app!")
-    const x = await departments.find({}).toArray();
-    res.send(x);
+    res.send(await departments.find({}).toArray())
 })
 
 module.exports = router;
