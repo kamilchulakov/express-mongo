4e92552e19412dac94e3fa717b6bac52d3266c70
ulyanovskk <mr1english4@gmail.com>
2022-01-28 16:00:40 +0300

    Add routes stubs

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const employers_router = require('./src/routes/employers.routes.js')
+const departments_router = require('./src/routes/departments.routes')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use('/api/employers', employers_router)
+app.use('/api/departments_router', departments_router)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+    console.log(`http://localhost:${port}/api/employers/ - to work with employers`)
+    console.log(`http://localhost:${port}/api/departments/ - to work with departments`)
 })
