87162d79019b3c16634e37ec4c8b6fe06645b685
ulyanovskk <mr1english4@gmail.com>
2022-01-28 22:41:17 +0300

    Add user and password

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -1,4 +1,4 @@
 const MongoClient = require("mongodb").MongoClient;
-const mongoClient = new MongoClient("mongodb://localhost:27017/");
+const mongoClient = new MongoClient("mongodb://user:password@localhost:27017/");
 
 module.exports = mongoClient
