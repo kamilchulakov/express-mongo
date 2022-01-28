f00d3f25be3cbee44305927855960c877608a705
ulyanovskk <mr1english4@gmail.com>
2022-01-28 22:41:45 +0300

    Implemented get

--- src/routes/departments.routes.js
+++ src/routes/departments.routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Not implemented yet!')
+router.get('/', async (req, res) => {
+    const departments = req.app.departments
+    if (departments === undefined) console.log("No departments collection in app!")
+    const x = await departments.find({}).toArray();
+    res.send(x);
 })
 
 module.exports = router;
