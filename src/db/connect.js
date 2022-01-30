57b61a33d85fa70939641806fa2d2045c0ee58df
ulyanovskk <mr1english4@gmail.com>
2022-01-31 00:07:16 +0300

    Update err messages

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 module.exports.connect = function() {
     mongoose.connect("mongodb://user:password@localhost:27017/")
-        .then(() => console.log("Successfully connect to MongoDB."))
-        .catch(err => console.error("Connection error", err))
+        .then(() => console.log("Successfully connected to MongoDB."))
+        .catch(err => console.error("Connection error :(", err))
 }
