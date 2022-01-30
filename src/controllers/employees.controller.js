6b0f5f908c615ff4d25fff6f6811a7ba15534023
ulyanovskk <mr1english4@gmail.com>
2022-01-30 23:13:58 +0300

    Update employees controller

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,4 +1,5 @@
 const employee = require("../models/employee")
+const departments = require("../models/department")
 module.exports.getAllEmployees = async function(req, res) {
     res.send(await employee.find({}))
 }
@@ -8,14 +9,22 @@ module.exports.getAllEmployeesByDepartment = async function(req, res) {
 }
 
 module.exports.getEmployeeById = async function(req, res) {
-    if (req.params.id === undefined) {
-        res.send("No id param")
-    }
     res.send(await employee.findById(req.params.id))
 }
 
 module.exports.setDepartmentToEmployee = async function(req, res) {
-    if (req.params.employer === undefined) {
-        res.send("No id param")
-    }
+    const employee = await employee.findById(req.body.employeeId)
+    const department = await departments.findById(req.body.departmentId)
+    await employee.update({department: department})
+    res.send("Update")
+}
+
+module.exports.createEmployee = async function(req, res) {
+    const newEmployee = await new employee(req.body).save()
+    res.json(newEmployee)
+}
+
+module.exports.deleteEmployee = async function(req, res) {
+    await employee.deleteOne({ _id: req.body.id})
+    res.send("Delete")
 }
--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const {getAllEmployees, getEmployeeById, getAllEmployeesByDepartment} = require("../controllers/employees.controller");
+const {getAllEmployees, getEmployeeById, getAllEmployeesByDepartment, setDepartmentToEmployee, deleteEmployee} = require("../controllers/employees.controller");
+const {createDepartment} = require("../controllers/departments.controller");
 const router = express.Router()
 
 function routeGetEmployeesByParams(req, res) {
@@ -8,6 +9,9 @@ function routeGetEmployeesByParams(req, res) {
 
 router.route('/employees')
     .get(routeGetEmployeesByParams)
+    .post(createDepartment)
+    .patch(setDepartmentToEmployee)
+    .delete(deleteEmployee)
 
 router.route('/employees/:id')
     .get(getEmployeeById)
